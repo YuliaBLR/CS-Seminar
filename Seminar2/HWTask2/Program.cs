6f1b68140db3d9215fb0b94561e6f23c6476965d
Yuliya <yulia.lukscha@yandex.ru>
2023-07-10 19:41:00 +0300

    Zadacha13

---
+++ Seminar2/HWTask2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 13
+// Напишите программу, которая выводит третью цифру заданного числа 
+//или сообщает, что третьей цифры нет.
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number < 100)
+{
+    Console.Write("Третьей цифры нет");
+}
+else
+{
+    while (number > 999)
+        {
+            number = number / 10;
+        }
+    Console.Write($"Третья цифра заданного числа: {number % 10}");
+}
