4aff8cb83b3e4d18fb59f94327fb6ce7585cf17a
Yuliya <yulia.lukscha@yandex.ru>
2023-07-10 18:50:08 +0300

    Zadacha 10

---
+++ Seminar2/HWTask1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 10
+// Напишите программу, которая принимает на вход трехзначное число
+// и на выходе показывает вторую цифру этого числа
+
+Console.Write("Введите трехзначное число: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number / 100 == 0 || number / 100 > 9)
+{
+  Console.Write("Число не трехзначное");  
+}
+else
+{
+    number = number / 10;
+    number = number % 10;
+    Console.Write($"Вторая цифра числа: {number}");
+}
