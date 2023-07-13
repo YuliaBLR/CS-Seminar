a417c72c6e4e3febececc0bf4ed9338388ae8385
Yuliya <yulia.lukscha@yandex.ru>
2023-07-13 19:51:44 +0300

    Zadacha 23

---
+++ Seminar3/HWTask3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) 
+//и выдаёт таблицу кубов чисел от 1 до N.
+
+void GetCube (int number)
+{
+    for (int i =1; i<=number; i++)
+    Console.Write($"{i*i*i} ");
+}
+
+//---------------------------------------------------------
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+GetCube(number);
