aeb37a1c3c75786a5ce170c2f4e2fe27a7ac648e
Yuliya <yulia.lukscha@yandex.ru>
2023-07-15 17:19:41 +0300

    Zadacha 19

--- Seminar3/HWTask1/Program.cs
+++ Seminar3/HWTask1/Program.cs
@@ -2,44 +2,35 @@
 // Напишите программу, которая принимает  на вход пятизначное число 
 // и проверяет, является ли оно палиндромом.
 
-
-
-
-//----------------------------------------------------------------
-
-Console.WriteLine("Введите пятизначное число: ");
-int number = int.Parse(Console.ReadLine());
-
-
-
-if (Math.Abs(number) > 9999 && Math.Abs(number) < 100000)
+void GetPalindrome(int number)
 {
-    int number5 = number % 10;
-    int number4 = number % 100;
-    int number3 = number % 1000;
-    int number2 = number % 10000;
-    int number1 = number % 100000;
-
-    if (number1 == number5 && number2 == number4 && number > 0)
-    {
-        
-        Console.Write("Число является полиндромом");
-    }
-    else
-    {
-       Console.Write("Число не является полиндромом"); 
-    }
-}
-else
+int numbertemp = number;
+if (Math.Abs(number) < 10000 || Math.Abs(number) > 99999)
 {
     Console.Write("Число не пятизначное");
 }
+else
+{
+    int number5 = numbertemp % 10; numbertemp = numbertemp / 10; 
+    int number4 = numbertemp % 10; numbertemp = numbertemp / 10; 
+    int number3 = numbertemp % 10; numbertemp = numbertemp / 10; 
+    int number2 = numbertemp % 10; numbertemp = numbertemp / 10; 
+    int number1 = numbertemp % 10; numbertemp = numbertemp / 10; 
 
+if (number > 9999 && number < 100000 && number1 == number5 && number2 == number4)
+{
 
+    Console.Write("Число является полиндромом");  
+}
+else
+{
+    Console.Write("Число не является полиндромом"); 
+}
+}
+}
 
+//------------------------------------------------------------
 
-
-
-
-
-  
+Console.Write("Введите пятизначное число: ");
+int number = int.Parse(Console.ReadLine());
+GetPalindrome(number);
