11db90e82cf35174686cbb63cb75fa304d66175e
Yuliya <yulia.lukscha@yandex.ru>
2023-07-17 00:08:17 +0300

    Zadacha 27

---
+++ Seminar4/HWTask2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 27.
+//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе
+
+int Sum (int number)
+{
+    int numbertemp = Math.Abs (number);
+    int resalt = 0;
+    while (numbertemp > 0)
+    {
+        int forsum = numbertemp % 10; // выуживам одну цифру из числа
+        numbertemp = numbertemp / 10; // убираем выуженную цифру из числа
+        resalt = resalt + forsum;
+    }
+    return resalt;
+
+}
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine($"Сумма цифр в числе: {Sum(number)} ");
