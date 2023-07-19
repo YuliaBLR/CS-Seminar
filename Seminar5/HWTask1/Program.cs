f8ee4b08f5aed33da84249fc053915a72051c969
Yuliya <yulia.lukscha@yandex.ru>
2023-07-20 00:58:02 +0300

    Zadacha 34

---
+++ Seminar5/HWTask1/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 34
+//Задайте массив заполненный случайными положительными трёхзначными числами. 
+//Напишите программу, которая покажет количество чётных чисел в массиве.
+
+//-----Заполнение массива случайными числами-----------------------
+
+void NewArray (int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(100,1000);
+    }
+}
+
+//------Печать массива--------------------------------------------------
+
+void PrintArray (int [] array)
+{
+        for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+//-------Количество четных чисел в массиве-------------------------------
+
+int EvenNumber (int[] array)
+{
+    int count = 0; 
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {count++;}
+    }
+    return count;
+}
+
+//------------------------------------------------------------------------
+
+int[] array = new int[4];
+NewArray(array);
+PrintArray(array);
+Console.WriteLine();
+Console.Write($"Количество четных чисел в массиве: {EvenNumber(array)}");
