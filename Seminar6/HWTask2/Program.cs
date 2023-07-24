e25cf239f6310f023838ebab40ed389a5c818608
Yuliya <yulia.lukscha@yandex.ru>
2023-07-24 19:33:51 +0300

    Zadacha43

---
+++ Seminar6/HWTask2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 43
+//Напишите программу, которая найдёт точку пересечения двух прямых,
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+//значения b1, k1, b2 и k2 задаются пользователем.
+
+Console.Write("Введите b1: ");
+double b1 = int.Parse(Console.ReadLine());
+Console.Write("Введите k1: ");
+double k1 = int.Parse(Console.ReadLine());
+Console.Write("Введите b2: ");
+double b2 = int.Parse(Console.ReadLine());
+Console.Write("Введите k2: ");
+double k2 = int.Parse(Console.ReadLine());
+
+double x = (b2 - b1) / (k1 - k2);
+double y = k1 * x + b1;
+Console.WriteLine($"Координаты точки пересечения: ({x},{y})");
